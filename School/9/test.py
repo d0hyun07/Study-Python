aa43c9d545b078929bd029fab363ca5ce8fb34dc
KDH <strongdh07@gmail.com>
2023-10-17 11:23:57 +0900

    2023-10-17

--- School/9/test.py
+++ School/9/test.py
@@ -1,3 +1,4 @@
+'''
 import time
 import random
 
@@ -22,9 +23,24 @@ for i in WORD_LIST:
     if c==i[index]:
       correct += 1
 
-total_len = len(i)
-c=correct/total_len*100
-e=(total_len-correct)/total_len*100
-speed=correct / end_time * 60
+  total_len = len(i)
+  c=correct/total_len*100
+  e=(total_len-correct)/total_len*100
+  speed=correct / end_time * 60
 
-print("속도 : {:0.2f} 정확도 : {:0.2f} 오타율 {:0.2f}".format(speed,c,e))
+  print("속도 : {:0.2f} 정확도 : {:0.2f} 오타율 {:0.2f}".format(speed,c,e))
+
+
+
+'''
+
+
+
+
+'''
+1. 클래스 붕어빵틀 = 서로 관련된 요소(변수, 기능)들을 한곳에 모아 관리하기 위한 틀
+2. 객체: 클래스를 이용해 만들어진 실체
+class name:
+  def __init__(self,...):
+          속성 정의
+'''
